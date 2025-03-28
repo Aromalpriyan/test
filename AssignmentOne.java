feea41afd044ee5cfd3c09fc03fc20b7b0d00c77
Aromalpriyan <aromalpriyan213@gmail.com>
2025-03-28 14:43:21 +0530

    Completed More Tasks

--- AssignmentOne.java
+++ AssignmentOne.java
@@ -1,9 +1,6 @@
-Java Assignment 1
-```Java 
 public class AssignmentOne {
     public static void main(String[] args){
         double shapeValue = 42.75;
         System.out.println(shapeValue);
     }
 }
-```
